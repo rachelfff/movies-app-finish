2ea4ef6ec307e16b6fc2c148879eb8153804f27c
rachelge <racheligenut121@gmail.com>
2020-03-15 12:58:44 +0200

    commit

--- src/app/movies/add-movie-modal/add-movie-modal.component.ts
+++ src/app/movies/add-movie-modal/add-movie-modal.component.ts
@@ -23,9 +23,9 @@ export class AddMovieModalComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.maxLength(30), Validators.pattern('^[a-zA-Z \-\']+')]],
       imdbLink: ['', [Validators.required, Validators.pattern(/http:\/\/(?:.*\.|.*)imdb.com\/(?:t|T)itle(?:\?|\/)(..\d+)/i)]],
       jpgLink: ['', [Validators.required , Validators.pattern(/http(|s):\/\/(?:.*\.|.*)m.media-amazon.com\/images*/i)]],
+      name: ['', [Validators.required, Validators.maxLength(30), Validators.pattern('^[a-zA-Z \-\']+')]],
       description: ['', Validators.required],
       rate: ['', Validators.required],
       length: ['', Validators.required]
@@ -34,12 +34,11 @@ export class AddMovieModalComponent implements OnInit {
       this.exitsName = this.exitsMoviesName.indexOf(movieName) > 0 ? true : false;
     });
   }
-
   checkValidForm() {
     if (this.form.valid && !this.exitsName && this.categoriesValue.length) {
       const sendDate = this.form.value;
-      sendDate['created'] = new Date().toDateString();
       sendDate['genres'] = this.categoriesValue;
+      sendDate['created'] = new Date().toDateString();
       this.activeModal.close(sendDate);
     }
   }
